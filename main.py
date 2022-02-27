0eeebdc4d811c1240b46fa5ba13e9f3d004c8b4d
Thibaut <th.landrein@gmail.com>
2022-02-27 10:44:36 +0100

    Widget pour la recherche de la date + affichage de l'image sur clic du bouton TODO: Améliorer l'UI + empêcher de rechercher les dates ultérieures à aujourd'hui ...

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ Created on Fri Feb 25 15:33:44 2022
 KEY  = "jw3DGr4LVXclcsVm3IpG8RFnZfCHWWDMoNJqlHpe"
 
 import requests
-from urllib.request import urlretrieve, urlopen
+from urllib.request import urlopen
 from pprint import PrettyPrinter
 from datetime import datetime
-from qtpy.QtWidgets import QLabel, QApplication, QWidget
+from qtpy.QtWidgets import QLabel, QApplication, QWidget, QPushButton, QGridLayout, QDateEdit
 from qtpy.QtGui import QPixmap
+
 pp = PrettyPrinter()
 
 
@@ -21,25 +22,45 @@ class App(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 image - pythonspot.com'
-        self.left = 10
-        self.top = 10
-        self.width = 640
-        self.height = 480
+        self.title = 'NASA Picture of the day search'
+        self.left = 300
+        self.top = 200
+        self.width = 1280
+        self.height = 960
+        self.layout = QGridLayout()
+        self.setLayout(self.layout)
+        self.calendar = None
         self.initUI()
     
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-    
-        # Create widget
-        label = QLabel(self)
-        pixmap = QPixmap()
-        data = data = urlopen(get_today_picture_url()).read()
-        pixmap.loadFromData(data)
-        label.setPixmap(pixmap)
-        self.resize(pixmap.width(),pixmap.height())
 
+        self.calendar = calendar = QDateEdit(self)
+        calendar.setGeometry(100, 100, 10, 10)
+
+        self.label = label = QLabel(self)
+        static_label = QLabel(self)
+        static_label.setText("Astronomy picture of the day : ")
+        btn = QPushButton(self)
+        btn.clicked.connect(self.display_picture)
+        btn.setText("Lancer la recherche")
+        
+        self.layout.addWidget(calendar, 0, 0, 1, 1)
+        self.layout.addWidget(btn, 0, 1, 1, 1)
+        self.layout.addWidget(static_label, 1, 0, 1, 1)
+        self.layout.addWidget(label, 2, 0, 1, 2)
+    
+    def _to_date(self):
+        date = self.calendar.date()
+        return f"{date.year()}-{date.month()}-{date.day()}"
+    
+    def display_picture(self):
+        date = self._to_date()
+        data = urlopen(get_picture_url(date)).read()
+        pixmap = QPixmap()
+        pixmap.loadFromData(data)
+        self.label.setPixmap(pixmap)
 
 def get_picture_url(date):
     """
@@ -48,12 +69,11 @@ def get_picture_url(date):
     """
     URL_APOD = "https://api.nasa.gov/planetary/apod"
     params = {
-        'api_key':KEY,
-        'date':date,
+        'api_key': KEY,
+        'date': date,
         'hd':'True'
     }
     response = requests.get(URL_APOD,params=params).json()
-    # pp.pprint(response)
     return response['url']
 
 def get_today_picture_url():
